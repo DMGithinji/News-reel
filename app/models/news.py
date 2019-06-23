37c5811caccac27cbe004521dd6c8d0ebb1b86b0
DMG <dmwaigithinji@gmail.com>
2019-06-23 17:03:53 +0300

    Add news model and its test and set up test news model to confirm that object is correctly initiated

---
+++ app/models/news.py
@@ -0,0 +1,14 @@
+class News:
+    '''
+    Categorized news class to define News Objects
+    '''
+
+    def __init__(self,sourceName, author,title,description,url,urlToImage,publishedAt,content):
+        self.sourceName = sourceName
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
