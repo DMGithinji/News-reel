ddbde279324bf9f6ada0f0f5b90c9e615e4f24a2
DMG <dmwaigithinji@gmail.com>
2019-06-23 11:40:16 +0300

    Add a route to display individual articles on views.py, create article.html and test rendering of content. Make route naming corrections

--- app/views.py
+++ app/views.py
@@ -9,25 +9,45 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    source = 'BBC News'
+    title = 'There are various examples of sources-based-on-category on this page'
+    general = 'General'
+    business = 'Business'
+    technology = 'Technology'
+    entertainment = 'Entertainment'
+    politics = 'Politics'
+    health = 'Health'
+    sports = 'Sports'
 
-    return render_template('index.html', source = source)
+    return render_template('index.html', title = title, general = general, business = business, technology = technology, sports = sports, health = health, entertainment = entertainment, politics = politics)
 
-@app.route('/source/<category>')
+@app.route('/source_category/<category>')
 def categorized_sources(category):
 
     '''
-    View categorized sources page function that returns sources that are categorized based on news topics
+    View categorized sources page function that returns sources that are categorized based on news topics.
+    Utilizes the source_base_url API endpoint
     '''
 
     return render_template('categorized-source.html',category = category)
 
 
-@app.route('/source/<category>/<source>')
-def source_articles(category, source):
+@app.route('/source/<sourceName>')
+def source_articles(sourceName):
 
     '''
-    View sources page function that returns articles from a particular source
+    View source page function that returns articles from a particular source
+    Utilizes the articles_base url API endpoint
+
     '''
 
-    return render_template('source-articles.html',category = category, source = source)
+    return render_template('source-articles.html', sourceName = sourceName)
+
+@app.route('/source/<sourceName>/<title>')
+def article(sourceName,title):
+
+    '''
+    View sources page function that returns an article from a particular source
+    Utilizes the articles_base url API endpoint
+    '''
+
+    return render_template('articles.html',title = title, sourceName = sourceName)
