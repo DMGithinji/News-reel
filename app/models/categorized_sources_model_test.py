c178a67888b7a3f752650a61d4080a584c7d2fc6
DMG <dmwaigithinji@gmail.com>
2019-06-23 16:44:56 +0300

    Test categorzed source model to confirm that object is correctly initiated

--- app/models/categorized_sources_model_test.py
+++ app/models/categorized_sources_model_test.py
@@ -15,5 +15,18 @@ class SourcesTest(unittest.TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.categorized_sources,Source))
 
+    def test_correct_movie_init(self):
+        '''
+        Test  that confirms that the object is instantiated correctly.
+
+        '''
+        self.assertEqual(self.categorized_sources.id, "abc-news")
+        self.assertEqual(self.categorized_sources.name, "ABC News")
+        self.assertEqual(self.categorized_sources.description, "Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.")
+        self.assertEqual(self.categorized_sources.urlLink, "https://abcnews.go.com")
+        self.assertEqual(self.categorized_sources.category, "general")
+        self.assertEqual(self.categorized_sources.language, "en")
+        self.assertEqual(self.categorized_sources.country, "us")
+
 if __name__ == '__main__':
     unittest.main()
